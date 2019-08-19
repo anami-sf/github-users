2b35b15daa807cf7956c3262386f3cced7694f12
anami823 <anami823@gmail.com>
2019-08-19 13:25:32 -0700

    Create index router

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+//Controller
+router.post('/', function(req, res, next) {
+  console.log(`username: ${req.body.username}`);
+  res.render('index');
 });
 
 module.exports = router;
